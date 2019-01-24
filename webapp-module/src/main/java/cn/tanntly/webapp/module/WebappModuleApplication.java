9eb609c8f2ffd326eda73daaa2d34465261a710b
tanntly <2264709841@qq.com>
2019-01-24 10:48:09 +0800

    first commit

---
+++ webapp-module/src/main/java/cn/tanntly/webapp/module/WebappModuleApplication.java
@@ -0,0 +1,14 @@
+package cn.tanntly.webapp.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappModuleApplication.class, args);
+    }
+
+}
+
